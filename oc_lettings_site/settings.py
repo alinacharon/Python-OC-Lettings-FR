d306ade1045e7420a65b103983a4da7772af670b
Alina Charon <alina.charon@gmail.com>
2024-11-29 15:13:24 +0100

    Update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['oc_lettings_site.onrender.com']
+ALLOWED_HOSTS = ['python-oc-lettings-fr-o8k3.onrender.com']
 
 
 # Application definition
