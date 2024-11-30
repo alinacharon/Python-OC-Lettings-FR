802eefcf87b6327451349c8c80f461b84a7c0111
Alina Charon <alina.charon@gmail.com>
2024-11-30 10:30:50 +0100

    Update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,4 +118,4 @@ STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'    
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
