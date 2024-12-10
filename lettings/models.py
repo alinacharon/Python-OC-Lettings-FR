76d5a5d0f31004d68f68bf3bbb9000724bbbddc7
Alina Charon <alina.charon@gmail.com>
2024-12-10 09:30:52 +0100

    Docstrings updated

--- lettings/models.py
+++ lettings/models.py
@@ -3,6 +3,11 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+    """
+    Represents a postal address with fields for number, street, city, state, zip code,
+    and country ISO code.
+    Used to store address details for lettings or other entities.
+    """
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
@@ -11,6 +16,10 @@ class Address(models.Model):
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     def __str__(self):
+        """
+        Returns a string representation of the address in the format:
+        'number street'.
+        """
         return f'{self.number} {self.street}'
 
     class Meta:
@@ -19,8 +28,15 @@ class Address(models.Model):
 
 
 class Letting(models.Model):
+    """
+    Represents a letting, which includes a title and a reference to an address.
+    Each letting is associated with a unique address.
+    """
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
+        """
+        Returns a string representation of the letting, which is its title.
+        """
         return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 
 class Profile(models.Model):
+    """
+    Represents a user profile that extends the built-in Django User model.
+    Each profile is associated with a single user and contains additional information.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profiles_profile")
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the profile, which is
+        the username of the associated user.
+        """
         return self.user.username
